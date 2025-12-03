53aecafbc9f9f7185df414c28e073e79dc860674
hiroxenz <hiroxenz14@gmail.com>
2025-12-03 16:36:51 +0700

    Update dashboard.js

--- api/dashboard.js
+++ api/dashboard.js
@@ -1,6 +1,5 @@
-import { getMonitor } from "../../utils/captchaStore.js";
+import { getMonitor } from "../utils/captchaStore.js";
 
 export default function handler(req,res){
-    const records = getMonitor();
-    res.json({ ok:true, records });
+    res.json({ ok:true, monitor:getMonitor() });
 }
